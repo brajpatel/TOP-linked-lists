640679a1a64c640e951bae81b16b28c97f0a9b19
brajpatel <brajeshpatel0002@gmail.com>
2023-01-23 11:52:07 +0000

    Create function skeletons

--- linkedList.js
+++ linkedList.js
@@ -9,4 +9,50 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    head() {
+        // return this.head;
+    }
+
+    tail() {
+        // if(!this.head) return null;
+
+        // let tail = this.head;
+        
+        // while(tail !== null) {
+        //     tail = tail.next;
+        // }
+
+        // return tail;
+    }
+
+    size() {
+        // let size = 0;
+        // let current = this.head;
+
+        // while(current !== null) {
+        //     current = current.next;
+        //     size++
+        // }
+
+        // return size;
+    }
+
+    append() {}
+
+    prepend() {}
+
+    at(index) {}
+    
+    pop() {}
+
+    contains(value) {}
+
+    find(value) {}
+
+    toString() {}
+
+    insertAt() {}
+    
+    removeAt() {}
 }
